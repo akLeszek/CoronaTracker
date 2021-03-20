1b956b54b7f755ec73ea0ef4eb15a3594b710f61
Adrian <akleszek99@gmail.com>
2021-03-20 15:29:02 +0100

    Corona Tracker Aplication

---
+++ src/test/java/com/akleszek/coronatrack/CoronaTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akleszek.coronatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
